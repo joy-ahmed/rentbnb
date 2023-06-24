7e87cdf22d042c31560053d2c78915323acc0cf1
Joy Ahmed <dev.joyahmed@gmail.com>
2023-06-24 22:01:59 +0600

    modifing images

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 
 const Avatar = () => {
   return (
-    <Image className="rounded-full" height='30' width='30' alt="Avatar" src={'/images/placeholder.jpg'} />
+    <Image className="rounded-full" height='30' width='30' alt="Avatar" src={'/static/images/placeholder.jpg'} />
   )
 }
 
