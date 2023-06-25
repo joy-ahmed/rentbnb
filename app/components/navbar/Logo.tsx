19a84484f46b3233b527a773330d0e29131eaf3b
Joy Ahmed <dev.joyahmed@gmail.com>
2023-06-25 06:39:48 +0600

    modifying image

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -1,10 +1,11 @@
 'use client'
 import Image from "next/image"
 import { useRouter } from "next/navigation"
+import BrandIcon from '/public/static/images/brand.png'
 
 const Logo = () => {
   return (
-    <Image alt="logo" className="hidden md:block cursor-pointer" height='100' width='100' src={'/static/images/brand.png'} />
+    <Image src={BrandIcon} alt="logo" className="hidden md:block cursor-pointer" height='100' width='100' />
   )
 }
 
